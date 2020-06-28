304e53de1e5d7e59a2da2d7fa2c45c5c1f66bcee
briantrang <41414918+briantrang@users.noreply.github.com>
2020-06-27 20:27:43 -0700

    Fixed some formatting issues

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Toolbar from './components/Navigation/Toolbar/Toolbar'
 import Hero from './components/Hero/Hero'
+import SectionHeader from './components/SectionHeader/SectionHeader'
 
 function App() {
   return (
     <div>
       <Toolbar />
       <Hero />
+      <SectionHeader />
     </div>
   );
 
---
+++ src/components/SectionHeader/SectionHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SectionHeader.scss'
+
+const SectionHeader = props => (
+
+    <div className="sectionContainer">
+
+    </div>
+
+);
+
+export default SectionHeader;
