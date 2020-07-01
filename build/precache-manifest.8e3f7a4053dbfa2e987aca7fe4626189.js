cc1c62329234a6470b9860672d743a43246faf3a
briantrang <41414918+briantrang@users.noreply.github.com>
2020-06-30 20:38:49 -0700

    another push

---
+++ build/precache-manifest.8e3f7a4053dbfa2e987aca7fe4626189.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96490a84d530708d11654c35d4c3a9b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6566200ffe4986097a3e",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "20713e9827e5ac351885",
+    "url": "/portfolio/static/css/main.1c62cb77.chunk.css"
+  },
+  {
+    "revision": "6566200ffe4986097a3e",
+    "url": "/portfolio/static/js/2.4ac220e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.4ac220e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20713e9827e5ac351885",
+    "url": "/portfolio/static/js/main.f1b4025f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "517a96ca8fdffda37353cbf75e3c1238",
+    "url": "/portfolio/static/media/DeepBlueImage.517a96ca.jpg"
+  },
+  {
+    "revision": "dda312e4072df78854d2b25b0ef5f595",
+    "url": "/portfolio/static/media/Logo 1000x400.dda312e4.png"
+  },
+  {
+    "revision": "5df06489f178f79435043469fa66aa21",
+    "url": "/portfolio/static/media/NoBSImage.5df06489.jpg"
+  },
+  {
+    "revision": "af0d47a8780fe6d69e47725a112b86d4",
+    "url": "/portfolio/static/media/SonsisiImage.af0d47a8.jpg"
+  },
+  {
+    "revision": "02aaf346ac6498b006363000f38e7e3c",
+    "url": "/portfolio/static/media/SuperbImage.02aaf346.jpg"
+  },
+  {
+    "revision": "8ddd372df6947f9f62d1006b6a3ef7d3",
+    "url": "/portfolio/static/media/bannerImage.8ddd372d.png"
+  }
+]);
