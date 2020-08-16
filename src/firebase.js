b768681af0cc562fe3af1ceec32a6860c2c6101c
Brian Trang <briantrang@Brians-MacBook-Pro.local>
2020-08-16 13:51:33 -0700

    update database url

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: "https://portfolio-6b831.firebaseio.com",
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
