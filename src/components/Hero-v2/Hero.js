2304ca1caf914f24027edf233cedd5dc3ac75749
Brian Trang <briantrang@Brians-MacBook-Pro.local>
2021-11-20 19:54:19 -0800

    portfolio redesign old work

---
+++ src/components/Hero-v2/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.scss";
+
+const Hero = (props) => (
+	<div className="heroContainer">
+		<div className="bannerImgContainer">
+			<div className="heroText">
+				<p>Hi, I'm Brian and I am a </p>
+			</div>
+		</div>
+	</div>
+);
+
+export default Hero;
