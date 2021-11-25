fbe23e532993143d614ff8dc5fa447185301f6bd
Brian Trang <briantrang@ymail.com>
2021-11-25 14:48:26 -0800

    WIP Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import './Hero.scss'
-import ReactRoatingText from 'react-rotating-text'
+import React from "react";
+import "./Hero.scss";
+import ReactRoatingText from "react-rotating-text";
+import heroImage from "../../assets/images/svg/coder.svg";
 
-const Hero = props => (
-
-    <div className="heroContainer" >
-        <div className="bannerImgContainer">
-            <div className="heroText">
-                <p>Hi, I'm Brian and I am a </p>
-                <span className="heroTextJob"><ReactRoatingText items={['Web Developer']}/></span>
-            </div>
-        </div>
-    </div>
+const Hero = (props) => (
+	<div className="heroContainer">
+		<div className="portfolio-container hero-contents">
+			<div className="hero-text">
+				<p className="hero-text-eyebrow">Hi, I am</p>
+				<p className="hero-text-heading">Brian Trang</p>
+				<p className="hero-text-job">
+					<ReactRoatingText items={["Full Stack Developer"]} />
+				</p>
+			</div>
+			<div class="hero-image-container">
+				<img className="hero-image" src={heroImage} alt="hero" />
+			</div>
+		</div>
+	</div>
 );
 
 export default Hero;
