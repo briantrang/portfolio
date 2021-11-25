dddefc3769104f4fb3922150d547549c435e98d1
Brian Trang <briantrang@ymail.com>
2021-11-25 15:52:15 -0800

    Updated Resume component

--- src/App.js
+++ src/App.js
@@ -7,105 +7,95 @@ import Toolbar from "./components/Navigation/Toolbar/Toolbar";
 import Hero from "./components/Hero/Hero";
 
 //Lazy loading components
-const SectionHeader = React.lazy(() =>
-  import("./components/SectionHeader/SectionHeader")
-);
+const SectionHeader = React.lazy(() => import("./components/SectionHeader/SectionHeader"));
 const Services = React.lazy(() => import("./components/Services/Services"));
 const SectionHeaderBGImage = React.lazy(() =>
-  import("./components/SectionHeader/SectionHeaderBGImage/SectionHeaderBGImage")
+	import("./components/SectionHeader/SectionHeaderBGImage/SectionHeaderBGImage")
 );
 
 const Projects = React.lazy(() => import("./components/Projects/Projects"));
-const PersonalProjects = React.lazy(() =>
-  import("./components/Projects/PersonalProjects/PersonalProjects")
-);
+const PersonalProjects = React.lazy(() => import("./components/Projects/PersonalProjects/PersonalProjects"));
 const Resume = React.lazy(() => import("./components/Resume/Resume"));
 const Contact = React.lazy(() => import("./components/Contact/Contact"));
 const Footer = React.lazy(() => import("./components/Footer/Footer"));
 
 function App() {
-  //database states
-  const [sectionHeader, setSectionHeader] = useState([]);
-  const [sectionHeaderBGImage, setSectionHeaderBGImage] = useState([]);
-  const [services, setServices] = useState([]);
-  const [projects, setProjects] = useState([]);
-  const [resume, setResume] = useState([]);
-  const [personalProjects, setPersonalProjects] = useState([]);
+	//database states
+	const [sectionHeader, setSectionHeader] = useState([]);
+	const [sectionHeaderBGImage, setSectionHeaderBGImage] = useState([]);
+	const [services, setServices] = useState([]);
+	const [projects, setProjects] = useState([]);
+	const [resume, setResume] = useState([]);
+	const [personalProjects, setPersonalProjects] = useState([]);
 
-  //call database and get data
-  useEffect(() => {
-    const databaseUrl = database.ref();
-    axios
-      .get(databaseUrl + ".json")
-      .then((res) => {
-        setSectionHeader(res.data.sectionHeader);
-        setSectionHeaderBGImage(res.data.sectionHeaderBGImage);
-        setServices(res.data.services);
-        setProjects(res.data.projects);
-        setResume(res.data.resume);
-        setPersonalProjects(res.data.personalProjects);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+	//call database and get data
+	useEffect(() => {
+		const databaseUrl = database.ref();
+		axios
+			.get(databaseUrl + ".json")
+			.then((res) => {
+				setSectionHeader(res.data.sectionHeader);
+				setSectionHeaderBGImage(res.data.sectionHeaderBGImage);
+				setServices(res.data.services);
+				setProjects(res.data.projects);
+				setResume(res.data.resume);
+				setPersonalProjects(res.data.personalProjects);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  return (
-    <div>
-      <Toolbar />
+	return (
+		<div>
+			<Toolbar />
+			<Hero />
+			<Suspense fallback={<p>Loading...</p>}>
+				<Resume title={resume.title} description={resume.description} link={resume.link} />
+			</Suspense>
 
-      <Hero />
+			<Suspense fallback={<p>Loading...</p>}>
+				<SectionHeader
+					title={sectionHeader.title}
+					content={sectionHeader.content}
+					subContentHeading={sectionHeader.subcontentHeading}
+				/>
+			</Suspense>
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <SectionHeader
-          title={sectionHeader.title}
-          content={sectionHeader.content}
-          subContentHeading={sectionHeader.subcontentHeading}
-        />
-      </Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				<Services servicesContent={services} />
+			</Suspense>
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <Services servicesContent={services} />
-      </Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				<SectionHeaderBGImage
+					title={sectionHeaderBGImage.title}
+					content={sectionHeaderBGImage.content}
+					background={sectionHeaderBGImage.background}
+				/>
+			</Suspense>
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <SectionHeaderBGImage
-          title={sectionHeaderBGImage.title}
-          content={sectionHeaderBGImage.content}
-          background={sectionHeaderBGImage.background}
-        />
-      </Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				<Projects projectsContent={projects} />
+			</Suspense>
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <Projects projectsContent={projects} />
-      </Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				<SectionHeader
+					title={personalProjects.title}
+					content={personalProjects.content}
+					subContentHeading={personalProjects.subcontentHeading}
+				/>
+				<PersonalProjects />
+			</Suspense>
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <Resume
-          title={resume.title}
-          description={resume.description}
-          link={resume.link}
-        />
-      </Suspense>
-      
-      <Suspense fallback={<p>Loading...</p>}>
-      <SectionHeader
-          title={personalProjects.title}
-          content={personalProjects.content}
-          subContentHeading={personalProjects.subcontentHeading}
-        />
-        <PersonalProjects />
-      </Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				<Contact />
+			</Suspense>
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <Contact />
-      </Suspense>
-
-      <Suspense fallback={<p>Loading...</p>}>
-        <Footer />
-      </Suspense>
-    </div>
-  );
+			<Suspense fallback={<p>Loading...</p>}>
+				<Footer />
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,43 +1,37 @@
-import React, { useState } from 'react'
-import './Resume.scss'
-import Modal from 'react-bootstrap/Modal'
-import Button from '../UI/Button/Button'
+import React, { useState } from "react";
+import "./Resume.scss";
+import Modal from "react-bootstrap/Modal";
 
-const Resume = props => {
-    const [show, setShow] = useState(false);
-    return(
-        <div>
-            <div className="resumeContainer">
-                <div className="p-3">
-                    <h2>{props.title}</h2>
-                    <p>{props.description}</p>
-                </div>
-                <Button style={{cursor: 'pointer'}} onClick={() => setShow(true)} >View my Resume</Button>
-                <Modal  
-                    show={show}
-                    onHide={() => setShow(false)}
-                    dialogClassName="modalContent"
-                >
-                    <Modal.Header closeButton>
-                    <Modal.Title className="modalTitle">
-                        Brian Trang's Resume - 
-                        <a href={props.link} target="_blank" rel="noopener noreferrer"> 
-                             &nbsp;Direct Link
-                            </a>
-                    </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <iframe 
-                            className="iframe"
-                            title="resume" 
-                            src={props.link} 
-                        > 
-                        </iframe>
-                    </Modal.Body>
-                </Modal>
-            </div>
-        </div>
-    );
-}
+const Resume = (props) => {
+	const [show, setShow] = useState(false);
+	return (
+		<div>
+			<div className="portfolio-container">
+				<div className="resume-content">
+					<div className="resume-heading">{props.title}</div>
+					<div className="resume-body">
+						{props.description} <br />
+						<button className="resume-button" onClick={() => setShow(true)}>
+							View my Resume <i class="fas fa-external-link-alt"></i>
+						</button>
+					</div>
+				</div>
+				<Modal show={show} onHide={() => setShow(false)} dialogClassName="modalContent">
+					<Modal.Header closeButton>
+						<Modal.Title className="modalTitle">
+							Brian Trang's Resume -
+							<a href={props.link} target="_blank" rel="noopener noreferrer">
+								&nbsp;Direct Link
+							</a>
+						</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<iframe className="iframe" title="resume" src={props.link}></iframe>
+					</Modal.Body>
+				</Modal>
+			</div>
+		</div>
+	);
+};
 
 export default Resume;
